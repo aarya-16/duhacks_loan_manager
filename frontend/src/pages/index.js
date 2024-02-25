38401db1fe95d568a09f03dd3d677c5a1e389d2b
kyash99252 <yashkadam99252@gmail.com>
2024-02-25 11:11:08 +0530

    Chat Page Frontend Part

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,5 @@
 export { default as Chat } from "./Chat";
+import { default as ChatSidebar } from "./ChatSidebar";
 export { default as Landing } from "./Landing";
 export { default as Login } from "./Login";
 export { default as Resources } from "./Resources";
